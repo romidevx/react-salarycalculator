066ae295df54d3f70e32541a0b9a38d70e07ebcc
romi-devx <romi_maximus@hotmail.com>
2020-10-20 20:37:51 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import "./App.css";
 
-function App() {
+  const App = () => {
+
+  const [salary,setSalary] = useState();
+  const [inss,setInss] = useState();
+  const [irrf,setIrrf] = useState();
+  const [liquidSalary,setLiquidSalary] = useState();
+
+  // CALULATE SALARY
+  const calculate = () => {
+    const calcInss = (11 / 100) * salary; // 11% TAX
+    const calcIrrf = (5 / 100)  * salary; // 5% TAX
+    const totalTaxes = calcIrrf + calcInss;
+
+    setInss('- $' + (calcInss * 10).toFixed(2));
+    setIrrf('- $' + (calcIrrf * 10).toFixed(2));
+    setLiquidSalary((Math.abs(salary - totalTaxes) * 10).toFixed(2));
+  }
+
+  // CLEAR STATE
+  const clearState = () => {
+    setSalary('');
+    setInss('');
+    setIrrf('');
+    setLiquidSalary('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+
+      <h1>Calculate Salary</h1>
+      <input placeholder='Enter gross salary amount..' value={salary} onChange={e => {
+        setSalary(e.target.value);
+        calculate();
+      }}/><br/>{inss ? <small> Salary Amount<br/></small> : ''}<br/>
+
+      <input placeholder='INSS' value={inss} /><br/>{inss ? <small>INSS 11% <br/></small> : ''}<br/>
+      <input placeholder='IRRF' value={irrf}/><br/>{irrf ? <small>IRRF 5% <br/></small> : ''}<br/>
+
+      {liquidSalary ?  <h4>Liquid salary: $ {liquidSalary}</h4> : ''}
+
+      <button onClick={clearState}>Clear values</button>
     </div>
   );
 }
+
 
 export default App;
